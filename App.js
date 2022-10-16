c484fff3609ece7d76c4f3a41fc54e1cb8504333
TaxManDan <mangomicrowave@gmail.com>
2022-10-15 18:52:52 -0600

    updated app.js

--- App.js
+++ App.js
@@ -1,49 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  const [text, setText, dist] = useState('');
+  const [time, setTime] = useState('5');
+  const [dist, setDist] = useState('5');
+  const [computed, setComputed] = useState(true);
+  const [pace, setPace] = useState('10')
   return (
     <View style={styles.container}>
       <Text>Welcome to the Running Pace Calculator!</Text>
       <TextInput
-      autoCorrect={true}
-      placeholder="in Miles"
-      onChangeText={newText => setText(newText)}
-      defaultValue={dist}
+      style = {styles.input}
+      placeholder = "Distance in Miles"
+      placeholderTextColor={"#16a63b"}
+      onChangeText = {newDist => setDist(newDist)}
+      defaultValue = {dist}
       />
       <TextInput
-      autoCorrect = {true}
-      placeholder = "Time"
+        style = {styles.input}
+        placeholder = "Time in Minutes"
+        placeholderTextColor={"#16a63b"}
+        onChangeText = {newText => setTime(newText)}
+        defaultValue = {time}
       />
-      <TextInput
-        style={{height: 40}}
-        placeholder="Type here to translate!"
-        onChangeText={newText => setText(newText)}
-        defaultValue={text}
+      <Button
+      onPress={()=>{
+        setComputed(false);
+        pace = computePace(text,dist);
+      }}
+      title={computed ?  "Compute Pace" : "Pace Computed"}
       />
-      
+      <TouchableOpacity 
+      style = {styles.square}
+      onPress ={()=> pace = computePace(time,dist)}
+      />
       <Text style={{padding: 10, fontSize: 42}}>
-        {computePace(text,dist)}
+        Distance: {dist}, Time: {time}, Pace {pace}
       </Text>
       <StatusBar style="auto" />
     </View>
   );
   
 }
-function computePace(times, dist){
+
+const computePace = (times, dist) =>{
   var time = parsefloat(times);
   var distance = parsefloat(dist);
-  var pace = time / distance;
+  var pace = distance / time;
+  
   return pace
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  square: {
+    width: 72,
+    height: 24,
+    backgroundColor: '#55BCF6',
+    opacity: 0.4,
+    borderRadius: 5,
+  },
+  input: {
+    borderwidth: 20,
+    borderColor: "#4b475b",
+    padding: 7,
+    opacity: 1,
+    borderRadius: 6,
+    margin: 7,
+    
   },
 });
